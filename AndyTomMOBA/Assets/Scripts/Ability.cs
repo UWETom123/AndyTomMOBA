4b52f51e655de311b716c7b51fa72d18a5a044c7
UWETom123 <tom.moreton@yahoo.co.uk>
2017-05-18 16:49:25 +0100

    Added cache files to git ignore and added Base hero and ability class

---
+++ AndyTomMOBA/Assets/Scripts/Ability.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Work in progress base class for abilities
+//All abilities will inherit from this class
+//Added by Tom Moreton 18/05/2017
+
+public class Ability : MonoBehaviour {
+
+    public enum m_AbilityType
+    {
+        Active, Passive, Toggle
+    }
+    public m_AbilityType m_myAbilityType;
+
+    public Texture2D m_AbilityIcon;
+
+    public float m_Damage;
+    public float m_Radius;
+    public float m_Range;
+    public float m_Cooldown;
+
+    public string m_keybind;
+    
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ AndyTomMOBA/Assets/Scripts/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Work in progress base class for heroes/students
+//All heroes will inherit from this class
+//Added by Tom Moreton 18/05/2017
+public class Hero : MonoBehaviour {
+
+    public string m_heroName;
+
+    public int m_health;
+    public int m_damage;
+
+    public Texture2D m_heroSprite;
+
+    public float m_movementSpeed;
+    public float m_jumpStrength;
+
+    public Ability[] m_heroAbilities;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
